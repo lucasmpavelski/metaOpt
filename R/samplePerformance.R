158b3d48ca66009f2b4ddc1fa2aeb8f58d66f6fb
Lucas Marcondes Pavelski <lmpavelski@yahoo.com.br>
2021-05-29 11:38:40 -0300

    Optional parallel on sample performance.

--- R/samplePerformance.R
+++ R/samplePerformance.R
@@ -4,7 +4,8 @@ sample_performance <- function(
                                problemSpace,
                                solve_function,
                                no_samples = 30,
-                               cache = NA) {
+                               cache = NA,
+                               parallel = TRUE) {
   stopifnot(no_samples >= 1)
   if (!is.na(cache) && file.exists(cache)) {
     return(readRDS(cache))
@@ -19,24 +20,44 @@ sample_performance <- function(
     unnest(seed)
   n_exper <- nrow(experiments)
   experiments <- experiments[sample(n_exper),]
-  result <- experiments %>%
-    future_pmap(function(problem, instance, seed, core) {
-      result <- solve_function(
-        algorithm = algorithm,
-        problem = problem,
-        config = config,
-        instance = instance,
-        seed = seed
-      )
-      tibble(
-        problem = list(problem),
-        instance = instance,
-        seed = seed,
-        result = list(result)
-      )
-    }, .options = furrr_options(seed = TRUE)) %>%
-    value() %>%
-    bind_rows()
+  if (parallel) {
+    result <- experiments %>%
+      future_pmap(function(problem, instance, seed, core) {
+        result <- solve_function(
+          algorithm = algorithm,
+          problem = problem,
+          config = config,
+          instance = instance,
+          seed = seed
+        )
+        tibble(
+          problem = list(problem),
+          instance = instance,
+          seed = seed,
+          result = list(result)
+        )
+      }, .options = furrr_options(seed = TRUE), .progress = TRUE) %>%
+      value() %>%
+      bind_rows()
+  } else {
+    result <- experiments %>%
+      pmap(function(problem, instance, seed, core) {
+        result <- solve_function(
+          algorithm = algorithm,
+          problem = problem,
+          config = config,
+          instance = instance,
+          seed = seed
+        )
+        tibble(
+          problem = list(problem),
+          instance = instance,
+          seed = seed,
+          result = list(result)
+        )
+      }) %>%
+      bind_rows()
+  }
   if (!is.na(cache)) {
     saveRDS(result, file(cache))
   }
